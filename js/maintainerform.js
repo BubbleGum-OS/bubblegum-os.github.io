48e1c26bd16b6423706677f9ea8219600e66ec20
Zadli <00vopoj00@gmail.com>
2021-08-22 09:42:56 +0300

    site: add dt link

--- js/maintainerform.js
+++ js/maintainerform.js
@@ -4,6 +4,7 @@ let githubname = document.getElementById("githubname");
 let whymaintain = document.getElementById("whymaintain");
 let git = document.getElementById("git");
 let deviceinfo = document.getElementById("deviceinfo");
+let dt = document.getElementById("dt");
 let notwork = document.getElementById("notwork");
 let maintainsend = document.getElementById("maintainsend");
 
@@ -16,6 +17,7 @@ maintainsend.onclick = function() {
     Why he(she) wants maintain ROM: <i>' + whymaintain.value + '</i>\n\
     Git level: <i>' + git.value + '</i>\n\
     Device info: <i>' + deviceinfo.value + '</i>\n\
+    Device trees: <i>' + dt.value + '</i>\n\
     Not work in test build: <i>' + notwork.value + '</i>';
     fetch("https://api.telegram.org/bot1974124923:AAHK6Fr34pqLQq-F5yrU1aYDaawNV-kcYhw/sendMessage?markdown=HTML&chat_id=-1001215972805&text="+encodeURIComponent(message_text)).then(alert("Sended to developers")).then(window.location.href="index.html");
 }
